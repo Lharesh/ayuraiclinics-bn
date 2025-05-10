74981245a46e105e9890d42e39f4f0991140ad35
Lharesh <hareshlekkala@gmail.com>
2025-05-10 12:24:10 +0530

    Deleted config.json

--- project4-code/app/(admin)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-// app/(admin)/_layout.tsx
-import { Stack } from 'expo-router';
-import { COLORS } from '@/constants/theme';
-
-export default function AdminLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }} />
-  );
-}
